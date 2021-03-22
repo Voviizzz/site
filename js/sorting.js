358fb0ffc501c319723fd1a9d5ab3a98089b4dd8
Voviizzz <ivovochka48@gmail.com>
2021-03-22 23:25:08 +0300

    версия  1366px

--- js/sorting.js
+++ js/sorting.js
@@ -22,3 +22,51 @@ function callback(evt) {
     }
   }
   
+  document.getElementById('raiting').addEventListener('click', raiting);
+function raiting(evt) {
+
+    let list = document.querySelector('.catalog_products');
+  
+    let items = list.childNodes;
+    let itemsArr = [];
+    for (let i in items) {
+      if (items[i].nodeType == 1) {
+        itemsArr.push(items[i]);
+      }
+    }
+  
+    itemsArr.sort(function(a, b) {
+      return parseFloat(a.getAttribute('raiting')) == parseFloat(b.getAttribute('raiting')) ?
+        0 :
+        (parseFloat(a.getAttribute('raiting')) < parseFloat(b.getAttribute('raiting')) ? 1 : -1);
+    });
+  
+    for (i = 0; i < itemsArr.length; ++i) {
+      list.appendChild(itemsArr[i]);
+    }
+  }
+  
+  document.getElementById('weight').addEventListener('click', callback);
+  function weight(evt) {
+  
+      let list = document.querySelector('.catalog_products');
+    
+      let items = list.childNodes;
+      let itemsArr = [];
+      for (let i in items) {
+        if (items[i].nodeType == 1) {
+          itemsArr.push(items[i]);
+        }
+      }
+    
+      itemsArr.sort(function(a, b) {
+        return parseFloat(a.getAttribute('weight')) == parseFloat(b.getAttribute('weight')) ?
+          0 :
+          (parseFloat(a.getAttribute('weight')) < parseFloat(b.getAttribute('weight')) ? 1 : -1);
+      });
+    
+      for (i = 0; i < itemsArr.length; ++i) {
+        list.appendChild(itemsArr[i]);
+      }
+    }
+    
